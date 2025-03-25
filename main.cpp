54882c1fc92bfc779b2519415611708661df41d1
g_rocket <georgiiromanishko@gmail.com>
2025-03-25 10:02:47 +0100

    feat: sum, diff, product, quot

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 int main() {
-    string name;
-    cout << "What is your name?" << endl;
-    cin >> name;
-    
-    cout << "My name is: " << name << endl;
+    int num1 = 10, num2 = 5;
+
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1 / num2;
+
+    cout << sum << " " << difference << " " << product << " " << quotient << endl;
+
     return 0;
 }
