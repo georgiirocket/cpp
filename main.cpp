0fb05168565b33db31aa2e8d5f40d7d240152899
g_rocket <georgiiromanishko@gmail.com>
2025-03-25 11:18:16 +0100

    feat(project): sqrt

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int num1 = 10, num2 = 5;
+    int a = 20;
 
-    num1++;//10
-    num2--;//5
-
-    --num1;//10
-    ++num2;//5
-
-    cout << num1 << " " << num2 << endl;
-
-
+    cout << sqrt(a) << endl;
     return 0;
 }
