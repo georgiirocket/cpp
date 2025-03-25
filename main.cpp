61d526afaef75bf35c039fa235efd47c2a85d292
g_rocket <georgiiromanishko@gmail.com>
2025-03-25 10:13:27 +0100

    feat: inc, decr

--- main.cpp
+++ main.cpp
@@ -4,26 +4,13 @@ using namespace std;
 int main() {
     int num1 = 10, num2 = 5;
 
-    int sum = num1 + num2;
-    int difference = num1 - num2;
-    int product = num1 * num2;
-    int quotient = num1 / num2;
+    num1++;//10
+    num2--;//5
 
-    cout << sum << " " << difference << " " << product << " " << quotient << endl;
+    --num1;//10
+    ++num2;//5
 
-    num1++;
-    num1 = num1 + 1;
-    num1 += 1;
-
-    num2--;
-    num2 = num2 - 1;
-    num2 -= 1;
-
-    num1 *= 2;
-    num1 /= 4;
-
-    --num1;
-    ++num2;
+    cout << num1 << " " << num2 << endl;
 
 
     return 0;
