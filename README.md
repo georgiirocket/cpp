14c8cf926753158b9d3c79ed64dc5a76bd66ba91
g_rocket <georgiiromanishko@gmail.com>
2025-03-26 17:44:21 +0100

    feat(project): two-dimensional array

--- README.md
+++ README.md
@@ -284,3 +284,28 @@ int main() {
     return 0;
 }
 ```
+
+## Two-dimensional array
+
+```c++
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+
+    arr[0][2] = 5;
+
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < len; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main() {
-    int a[5];
-    int b[4] = {1,2,3,4};
+    int arr[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
 
-    int len = sizeof(b) / sizeof(b[0]);
+    arr[0][2] = 5;
+
+    int len = sizeof(arr) / sizeof(arr[0]);
 
     for(int i = 0; i < len; i++) {
-        cout << b[i] << " ";
+        for(int j = 0; j < 3; j++) {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
     }
 
     return 0;
