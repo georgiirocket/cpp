3d532b71e735a52d75b0bafa9d9139d9eae4974e
g_rocket <georgiiromanishko@gmail.com>
2025-03-26 11:48:11 +0100

    feat(project): array lenght

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ int main() {
     int a[5];
     int b[4] = {1,2,3,4};
 
-    cout << b[1] << endl;
+    int len = sizeof(b) / sizeof(b[0]);
+
+    for(int i = 0; i < len; i++) {
+        cout << b[i] << " ";
+    }
 
     return 0;
 }
