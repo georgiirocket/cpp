703813f5d45bb9ce7bec1682159eba5d919ea20d
g_rocket <georgiiromanishko@gmail.com>
2025-03-27 13:04:58 +0100

    feat(project): dynamic struct

--- README.md
+++ README.md
@@ -435,3 +435,58 @@ int main() {
     return 0;
 }
 ```
+
+## Dynamic struct
+
+```c++
+#include <iostream>
+using namespace std;
+
+struct node {
+    int value;
+    node* next;
+};
+
+node* head = nullptr;
+node* temp;
+
+void add(int value) {
+    if(head == nullptr) {
+        head = new node;
+
+        head->value = value;
+        head->next = nullptr;
+
+        temp = head;
+
+        return;
+    }
+
+    head->next = new node;
+    head = head->next;
+    head->value = value;
+}
+
+void print() {
+    node* t = temp;
+
+    while (t != nullptr)
+    {
+        cout << t->value << " ";
+
+        t = t->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+    add(1);
+    add(2);
+    add(3);
+
+    print();
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 using namespace std;
 
+struct node {
+    int value;
+    node* next;
+};
+
+node* head = nullptr;
+node* temp;
+
+void add(int value) {
+    if(head == nullptr) {
+        head = new node;
+
+        head->value = value;
+        head->next = nullptr;
+
+        temp = head;
+
+        return;
+    }
+
+    head->next = new node;
+    head = head->next;
+    head->value = value;
+}
+
+void print() {
+    node* t = temp;
+
+    while (t != nullptr)
+    {
+        cout << t->value << " ";
+
+        t = t->next;
+    }
+
+    cout << endl;
+}
+
 int main() {
-    int a = 5;
-    int* ptr = &a;
+    add(1);
+    add(2);
+    add(3);
 
-    cout << &a << endl;
-    cout << *ptr << endl;
-
-    //Dynamic memory
-    int* ptrOne = new int;
-    *ptrOne = 8;
-
-    delete ptrOne;
-
-    ptrOne = nullptr;
+    print();
 
     return 0;
 }
