ca48f8ccbdc91d3fa7873fc15dc277832eed5e87
g_rocket <georgiiromanishko@gmail.com>
2025-03-27 15:34:46 +0100

    feat(project): queue

--- README.md
+++ README.md
@@ -540,3 +540,73 @@ int main() {
     return 0;
 }
 ```
+
+## Dynamic struct (Queue)
+
+```c++
+#include <iostream>
+using namespace std;
+
+struct node {
+    int value;
+    node* next;
+};
+
+node* head = nullptr;
+node* tail = nullptr;
+
+void enqueue (int value) {
+    if(head == nullptr) {
+        head = new node;
+
+        head->value = value;
+        head->next = nullptr;
+
+        tail = head;
+
+        return;
+    }
+
+    tail->next = new node;
+    tail = tail->next;
+    tail->value = value;
+    tail->next = nullptr;
+}
+
+void dequeue () {
+    node* temp = head->next;
+
+    delete head;
+
+    head = temp;
+}
+
+void print() {
+    node* temp = head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    
+    cout << endl;
+}
+
+int main() {
+    enqueue(1);
+    enqueue(9);
+    enqueue(6);
+
+    print();
+
+    dequeue();
+
+    print();
+
+    cout << "Head: " << head->value << endl;
+    cout << "Tail: " << tail->value << endl;
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -7,39 +7,59 @@ struct node {
 };
 
 node* head = nullptr;
+node* tail = nullptr;
 
-void push(int value) {
+void enqueue (int value) {
     if(head == nullptr) {
         head = new node;
 
         head->value = value;
         head->next = nullptr;
 
+        tail = head;
+
         return;
     }
 
-    node* temp = new node;
-
-    temp->value = value;
-    temp->next = head;
-
-    head = temp;
+    tail->next = new node;
+    tail = tail->next;
+    tail->value = value;
+    tail->next = nullptr;
 }
 
-void pop() {
+void dequeue () {
     node* temp = head->next;
-    
+
     delete head;
 
     head = temp;
 }
 
-int main() {
-    push(2);
-    push(3);
-    pop();
+void print() {
+    node* temp = head;
 
-    cout << head->value << endl;
+    while (temp != nullptr)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    
+    cout << endl;
+}
+
+int main() {
+    enqueue(1);
+    enqueue(9);
+    enqueue(6);
+
+    print();
+
+    dequeue();
+
+    print();
+
+    cout << "Head: " << head->value << endl;
+    cout << "Tail: " << tail->value << endl;
 
     return 0;
 }
